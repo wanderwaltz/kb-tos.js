405c01d58664decffa70bcfd6dd442c7c4d5bf51
wanderwaltz <wanderwaltz@gmail.com>
2014-06-14 02:34:34 +0700

    Small geometry classes refactoring.

--- scripts/graphics/kb.graphics.geometry.ts
+++ scripts/graphics/kb.graphics.geometry.ts
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 Egor Chiglintsev. All rights reserved.
 //
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// KB.Graphics.Geometry
+////////////////////////////////////////////////////////////////////////////////////////////////////
 module KB.Graphics {
   export class Point {
     constructor(x: number, y: number) {
@@ -13,9 +16,15 @@ module KB.Graphics {
       this.y = y;
     }
 
+    public copy(): Point {
+      return new Point(this.x, this.y);
+    }
+
     public x: number;
     public y: number;
   }
+
+
 
   export class Size {
     constructor(width: number, height: number) {
@@ -23,15 +32,24 @@ module KB.Graphics {
       this.height = height;
     }
 
+    public copy(): Size {
+      return new Size(this.width, this.height);
+    }
+
     public width: number;
     public height: number;
   }
 
 
+
   export class Rect {
     constructor(origin: Point, size: Size) {
-      this._origin = new Point(origin.x, origin.y);
-      this._size = new Size(size.width, size.height);
+      this._origin = origin.copy();
+      this._size = size.copy();
+    }
+
+    public copy(): Rect {
+      return new Rect(this._origin, this._size);
     }
 
     public get origin(): Point {
@@ -43,44 +61,74 @@ module KB.Graphics {
     }
 
     public set origin(o: Point) {
-      this._origin = new Point(o.x, o.y);
+      this._origin = o.copy();
     }
 
     public set size(s: Size) {
-      this._size = new Size(s.width, s.height);
+      this._size = s.copy();
     }
 
     private _origin: Point;
     private _size: Size;
   }
 
+
   export interface PointFactory {
     (x: number, y: number): Point;
     zero: Point;
   }
 
+
   export interface SizeFactory {
     (width: number, height: number): Size;
+    zero: Size;
   }
+
 
   export interface RectFactory {
     (origin: Point, size: Size): Rect;
+    empty: Rect;
+    zero: Rect;
   }
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// KB.Graphics.Point factory
+////////////////////////////////////////////////////////////////////////////////////////////////////
 var point = <KB.Graphics.PointFactory>function(x: number, y: number): KB.Graphics.Point {
   return new KB.Graphics.Point(x, y);
 }
 
 point.zero = new KB.Graphics.Point(0,0);
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// KB.Graphics.Size factory
+////////////////////////////////////////////////////////////////////////////////////////////////////
 var size = <KB.Graphics.SizeFactory>function(width: number, height: number): KB.Graphics.Size {
   return new KB.Graphics.Size(width, height);
 }
 
+size.zero = new KB.Graphics.Size(0,0);
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var rect = <KB.Graphics.RectFactory>function(origin: KB.Graphics.Point, size: KB.Graphics.Size): KB.Graphics.Rect {
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// KB.Graphics.Rect factory
+////////////////////////////////////////////////////////////////////////////////////////////////////
+var rect = <KB.Graphics.RectFactory>function(origin: KB.Graphics.Point,
+                                             size: KB.Graphics.Size): KB.Graphics.Rect {
   return new KB.Graphics.Rect(origin, size);
 }
+
+rect.empty = new KB.Graphics.Rect(point.zero, size.zero);
+rect.zero = rect.empty;
+////////////////////////////////////////////////////////////////////////////////////////////////////
